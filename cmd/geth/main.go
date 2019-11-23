552873add85a6cf7ac3a992ae7834aa80fb98cca
mimota <1643160505@qq.com>
2019-11-23 10:40:51 +0800

    change inserttime logic

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -255,7 +255,7 @@ func init() {
 
 func main() {
 	//args := []string{"test", "--syncmode", "fast", "--cache", "2048", "--datadir", "/home/mimota/data", "console"}
-	args := []string{"test", "copydb", "--syncmode", "fast", "--cache", "4096", "/home/mimota/ethereum_data_19/geth/chaindata", "/home/mimota/ethereum_data_19/geth/chaindata/ancient"}
+	args := []string{"test", "copydb", "--syncmode", "fast", "--cache", "1024", "/home/mimota/ethereum_data_19/geth/chaindata", "/home/mimota/ethereum_data_19/geth/chaindata/ancient"}
 	//if err := app.Run(os.Args); err != nil {
 	if err := app.Run(args); err != nil {
 
