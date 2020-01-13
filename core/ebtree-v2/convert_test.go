2a9d0a041861c6474d77084fe7fa7731c051211f
mimota <1643160505@qq.com>
2020-01-13 11:35:58 +0800

    重写了int和byte数组转化的方法，速度还可以，并且去除了前导0

---
+++ core/ebtree-v2/convert_test.go
@@ -0,0 +1,72 @@
+package ebtree_v2
+
+import (
+	"encoding/binary"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestConvertInt2Byte(t *testing.T) {
+	size := 10000000
+
+	array := make([]uint64, size)
+	for i := 0; i < size; i++ {
+		array[i] = rand.Uint64()
+	}
+
+	tt := time.Now()
+	for i := 0; i < size; i++ {
+		before := array[i]
+		buf := IntToBytes(before)
+		if len(buf) > 0 && buf[0] == 0 {
+			fmt.Println(buf)
+		}
+		after := BytesToInt(buf)
+		if before != after {
+			fmt.Println("error", before, after)
+			fmt.Println("-----------------")
+		}
+	}
+	fmt.Printf("finished, timeElapsed: %f s\n", time.Now().Sub(tt).Seconds())
+}
+
+func TestConvertInt2ByteV2(t *testing.T) {
+	size := 100
+
+	array := make([]uint64, size)
+	for i := 0; i < size; i++ {
+		array[i] = rand.Uint64()
+	}
+
+	tt := time.Now()
+	for i := 0; i < size; i++ {
+		before := array[i]
+		fmt.Println("before", before)
+		buf := IntToBytes(before)
+		fmt.Println("buffer", buf)
+		fmt.Println("origin", IntToBytesOrigin(before))
+		after := BytesToInt(buf)
+		fmt.Println(after)
+	}
+	fmt.Printf("finished, timeElapsed: %f s\n", time.Now().Sub(tt).Seconds())
+}
+
+func TestNilByteArray(t *testing.T) {
+	var buf = make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, 0)
+	fmt.Println(buf)
+
+	var buf2 = PutUint64(0)
+	fmt.Println(buf2)
+
+	tt := time.Now()
+	var size = 1000000
+	for i := 0; i < size; i++ {
+		var array []int
+		array = append(array, 1)
+	}
+	fmt.Printf("finished, timeElapsed: %f s\n", time.Now().Sub(tt).Seconds())
+
+}
--- core/ebtree-v2/convertutils.go
+++ core/ebtree-v2/convertutils.go
@@ -7,7 +7,7 @@ import (
 )
 
 //todo fill TD struct
-func Convert2IdentifierData(blockno int, txIndex int) []byte{
+func Convert2IdentifierData(blockno int, txIndex int) []byte {
 	return []byte(string(blockno) + "," + string(txIndex))
 }
 
@@ -17,11 +17,55 @@ func StringToBig(a string) hexutil.Big {
 }
 
 func IntToBytes(i uint64) []byte {
+	var buf = PutUint64(i)
+	return buf
+}
+
+func IntToBytesOrigin(i uint64) []byte {
 	var buf = make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, i)
 	return buf
 }
 
-func BytesToInt(b []byte) (i uint64) {
-	return binary.BigEndian.Uint64(b)
+func PutUint64(v uint64) []byte {
+	if v == 0 {
+		return make([]byte, 1)
+	}
+
+	var b = make([]byte, 8)
+	_ = b[7] // early bounds check to guarantee safety of writes below
+	b[0] = byte(v >> 56)
+	b[1] = byte(v >> 48)
+	b[2] = byte(v >> 40)
+	b[3] = byte(v >> 32)
+	b[4] = byte(v >> 24)
+	b[5] = byte(v >> 16)
+	b[6] = byte(v >> 8)
+	b[7] = byte(v)
+
+	var length int
+	for i := 0; i < 8; i++ {
+		if b[i] == 0 {
+			length++
+		} else {
+			break
+		}
+	}
+
+	return b[length:]
+}
+
+func BytesToInt(data []byte) (num uint64) {
+	//return binary.BigEndian.Uint64(b)
+	var ret uint64 = 0
+	var len int = len(data)
+	var i uint = 0
+	for i = 0; i < uint(len); i++ {
+		ret = ret<<(8) | uint64(data[i])
+	}
+	return ret
+}
+
+func BytesToIntOrigin(data []byte) (num uint64) {
+	return binary.BigEndian.Uint64(data)
 }
